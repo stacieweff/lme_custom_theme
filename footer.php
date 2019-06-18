73fee819590c8ea043e8e784cccd0f6003d99daf
weffelms <stacie.weffelmeyer@wwt.com>
2019-06-18 13:14:33 -0500

    footer and style updates

--- footer.php
+++ footer.php
@@ -1,14 +1,27 @@
 <footer class="site-footer">
-  <nav class="site-nav">
-    <?php
-        $args = array(
-          'theme_location' => 'footer'
-        );
-        ?>
-        <?php wp_nav_menu( $args ); ?>
-  </nav>
+  <div class="footer-nav-links">
+    <nav class="site-nav">
+      <?php
+          $args = array(
+            'theme_location' => 'footer'
+          );
+          ?>
+          <?php wp_nav_menu( $args ); ?>
+    </nav>
+  </div>
 
-  <p><?php bloginfo('name'); ?> - &copy; <?php echo date('Y'); ?></p>
+  <div class="footer-company-info">
+    <div class="footer-left">
+      <div class="footer-logo">
+        <img src="http://localhost:8888/new_lme_company/wp-content/uploads/2018/10/lme_logo.jpg" width="50px"/>
+      </div>
+      <div class="footer-info">
+        <p><?php bloginfo('name'); ?> - &copy; <?php echo date('Y'); ?> All Rights Reserved.</p>
+      </div>
+    </div>
+    <div class="footer-right">
+    </div>
+  </div>
 </footer>
 </div> <!--end container-->
 
