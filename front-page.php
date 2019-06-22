6f3060880a80bf958dba62ed3b1c3aea9dc2dbfc
weffelms <stacie.weffelmeyer@wwt.com>
2019-06-22 15:51:20 -0500

    added home page content

--- front-page.php
+++ front-page.php
@@ -31,23 +31,21 @@
         <div class="news-block-heading"><h1>Recent News</h1></div>
         <div class="news-block-container">
           <?php
-          //opinion posts start here
+          //news posts start here
           //&orderby=title&order=ASC
-          $deliveryPosts = new WP_Query('cat=2&posts_per_page=6');
-                 if ($deliveryPosts->have_posts()) :
-                   while ($deliveryPosts->have_posts()) : $deliveryPosts->the_post(); ?>
+          $newsPosts = new WP_Query('cat=3&posts_per_page=6');
+                 if ($newsPosts->have_posts()) :
+                   while ($newsPosts->have_posts()) : $newsPosts->the_post(); ?>
 
                    <div class="news-block">
                      <div class="post <?php if ( has_post_thumbnail() ) { ?>has-thumbnail <?php } ?>">
                        <div class="background-thumbnail"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('small-thumbnail'); ?></a>
                          <div class="text-container">
                            <div class="news-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
-                           <div class="news-excerpt"><?php the_excerpt(); ?></div>
+                           <!-- <div class="news-excerpt"><?php the_excerpt(); ?></div> -->
                          </div>
-
                        </div>
                      </div>
-
                    </div>
 
                     <!-- <article class="post <?php if ( has_post_thumbnail() ) { ?>has-thumbnail <?php } ?>">
@@ -72,24 +70,69 @@
         </div>
       </div>
 
-      <!-- <div class="one-half last">
-        <?php
-        $newsPosts = new WP_Query('cat=3&posts_per_page=2');
-               if ($newsPosts->have_posts()) :
-                 while ($newsPosts->have_posts()) : $newsPosts->the_post(); ?>
+      <div class="apparatus-dealer-wrapper">
+        <div class="apparatus-dealer-header"><h1>Apparatus Dealers</h1></div>
+        <div class="apparatus-dealer-container">
+              <?php
+              $appdealersPosts = new WP_Query('cat=11&posts_per_page=4');
+                    if ($appdealersPosts->have_posts()) :
+                      while ($appdealersPosts->have_posts()) : $appdealersPosts->the_post(); ?>
 
-                 <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                 <?php the_excerpt(); ?>
 
-                 <?php endwhile;
 
-               else :
-                 echo '<p>No content found</p>';
+                  <div class="apparatus-dealer-block">
+                     <div class="post <?php if ( has_post_thumbnail() ) { ?>has-thumbnail <?php } ?>">
+                       <div class="background-thumbnail"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('small-thumbnail'); ?></a>
+                       </div>
+                     </div>
+                   </div>
 
-               endif;
-               wp_reset_postdata();
-        ?>
-      </div> -->
+                      <!-- <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2> -->
+                      <!-- <?php the_excerpt(); ?> -->
+
+                      <?php endwhile;
+
+                    else :
+                      echo '<p>No content found</p>';
+
+                    endif;
+                    wp_reset_postdata();
+              ?>
+        </div>
+      </div>
+
+      <div class="vendor-wrapper">
+        <div class="vendor-header"><h1>Featured Vendors</h1></div>
+        <div class="vendor-container">
+              <?php
+              $featuredVendorsPosts = new WP_Query('tag=featured-product&posts_per_page=4');
+                    if ($featuredVendorsPosts->have_posts()) :
+                      while ($featuredVendorsPosts->have_posts()) : $featuredVendorsPosts->the_post(); ?>
+
+
+
+                  <div class="vendor-block">
+                     <div class="post <?php if ( has_post_thumbnail() ) { ?>has-thumbnail <?php } ?>">
+                       <div class="background-thumbnail"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('small-thumbnail'); ?></a>
+                       </div>
+                     </div>
+                   </div>
+
+                      <!-- <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2> -->
+                      <!-- <?php the_excerpt(); ?> -->
+
+                      <?php endwhile;
+
+                    else :
+                      echo '<p>No content found</p>';
+
+                    endif;
+                    wp_reset_postdata();
+              ?>
+        </div>
+      </div>
+
+
     </div>
 </div>
 
