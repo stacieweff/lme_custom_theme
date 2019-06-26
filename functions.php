c297b8eb1803bcca73a22a0c78959701ee15400d
weffelms <stacie.weffelmeyer@wwt.com>
2019-06-26 12:19:28 -0500

    category pages

--- functions.php
+++ functions.php
@@ -17,4 +17,24 @@ register_nav_menus(array(
   add_image_size('small-thumbnail', 180, 120, true);
   add_image_size('banner-image', 920, 210, array('left', 'top'));
 
+  //Add post format support
+  add_theme_support('post-formats', array('aside', 'gallery', 'link'));
+
+//get top get_top_ancestor
+function get_top_ancestor_id() {
+  global $post;
+  if ($post->post_parent) {
+    $ancestors = array_reverse(get_post_ancestors($post->ID));
+    return $ancestors[0];
+  }
+  return $post->ID;
+}
+
+//does page have children
+function has_children() {
+  global $post;
+
+  $pages = get_pages('child_of=' . $post->ID);
+  return count($pages);
+}
 ?>
