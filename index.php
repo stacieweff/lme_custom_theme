54e36c52a4b79bb302b135460431e36053d48a2d
weffelms <stacie.weffelmeyer@wwt.com>
2019-08-25 16:48:26 -0500

    single category item pages and breadcrumb

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php
-get_header();
+get_header(); ?>
 
+<div class="site-content clearfix">
+  <div class="main-column">
+    <?php
+    if (have_posts()) :
+      while (have_posts()) : the_post();
+
+      get_template_part('category', get_post_format());
+
+      endwhile;
+
+    else :
+      echo '<p>No content found</p>';
+
+    endif; ?>
+  </div>
+  <!-- <?php get_sidebar(); ?> -->
+</div>
+
+
+
+<?php
 get_footer();
 ?>
---
+++ single.php
@@ -0,0 +1,42 @@
+<?php
+
+get_header();
+
+if (have_posts()) :
+  while (have_posts()) : the_post(); ?>
+
+  <article class="post">
+  <div class="content-wrapper-cat">
+    <div class="breadcrumb">
+			<?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?>
+		</div>
+    <h1><?php the_title(); ?></h1>
+    <p class="post-info">
+      <?php the_time('F jS, Y, g:i a'); ?> | by <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author(); ?></a> | Posted in
+      <?php
+      $categories = get_the_category();
+      $separator = ", ";
+      $output = '';
+
+      if ($categories) {
+        foreach ($categories as $category) {
+          $output .= '<a href="' . get_category_link($category->term_id) . '">' . $category->cat_name . '</a>' . $separator;
+        }
+        echo trim($output, $separator);
+      }
+      ?>
+    </p>
+
+    <!-- <?php the_post_thumbnail('banner-image'); ?> -->
+    <?php the_content(); ?>
+    </div>
+    <div class="background-image-stripe"> </div>
+  </article>
+
+  <?php endwhile;
+else :
+  echo '<p>No content found</p>';
+endif;
+
+get_footer();
+?>
