e47bd2b78a67419d7379e57a87ed67618b1c5225
weffelms <stacie.weffelmeyer@wwt.com>
2019-08-28 22:17:27 -0500

    product page product cards, and sub menu, sub menu to product template page, style changes

--- page-1582.php
+++ page-1582.php
@@ -1,6 +1,5 @@
 <?php
 get_header();
-
 if (have_posts()) :
   while (have_posts()) : the_post(); ?>
 
@@ -10,44 +9,52 @@ if (have_posts()) :
         <!-- <h2><?php the_title(); ?></h2> -->
       </div>
       <div class="text-column">
-        <?php the_content(); ?>
-      </div>
-      <?php
-      $args = array(
-        'child_of' => get_top_ancestor_id(),
-        'title_li' => ''
-      )
+      <?php 
+        the_content();
       ?>
-      <?php wp_list_pages($args) ?>
-      <div class="home-columns clearfix">
-        <div class="news-block-wrapper">
-          <div class="news-block-container">
+      </div>
+
+      <?php if ( has_children() OR $post->post_parent > 0 ) { ?>
+        <nav class="site-nav children-links clearfix">
+        <span class="parent-link"><a href="<?php echo get_the_permalink(get_top_ancestor_id()) ?>"><?php echo get_the_title(get_top_ancestor_id()) ?></a></span>
+        <ul>
+          <?php
+            $args = array(
+              'child_of' => get_top_ancestor_id(),
+              'title_li' => ''
+            )
+            ?>
+            <?php wp_list_pages($args) ?>
+        </ul>
+      </nav>
+      <?php } ?>
+      <h2>Featured Products</h2>
+      <div class="products-container">
             <?php
             //products posts start here
             $productPosts = new WP_Query('cat=4');
             if ($productPosts->have_posts()) :
               while ($productPosts->have_posts()) : $productPosts->the_post(); ?>
-              <div class="news-block">
-                <div class="post <?php if ( has_post_thumbnail() ) { ?>has-thumbnail <?php } ?>">
-                  <div class="background-thumbnail"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium-thumbnail'); ?></a>
+                  <div class="products-card">
+                     <div class="post <?php if ( has_post_thumbnail() ) { ?>has-thumbnail <?php } ?>">
+                       <div class="background-thumbnail"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium-thumbnail'); ?></a>
+                       </div>
+                     </div>
+                     <div class="text-container">
+                           <div class="news-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
+                      </div>
                   </div>
-                </div>
-                <div class="text-container">
-                  <div class="news-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
-                    <!-- <div class="news-excerpt"><?php the_excerpt(); ?></div> -->
-                  </div>
-                </div>
-                   <?php endwhile;
+              <?php endwhile;
                  else :
                    echo '<p>No content found</p>';
                  endif;
                  wp_reset_postdata();
               ?>
-          </div>
-      </div>
+              </div>
     </div>
     <div class="background-image-stripe"> </div>
   </article>
+
 
   <?php endwhile;
 
