a35e5f1dc7c6948bafd44e9bfedb0235fdb46b68
weffelms <stacie.weffelmeyer@wwt.com>
2019-09-04 23:15:49 -0500

    search results

--- page.php
+++ page.php
@@ -8,14 +8,20 @@ if (have_posts()) :
 
   <article class="post page">
     <div class="content-wrapper">
-    <span class="parent_link"><a href="<?php echo get_the_permalink(get_top_ancestor_id()) ?>"><?php echo get_the_title(get_top_ancestor_id()) ?></a></span>
-    <?php
-      $args = array(
-        'child_of' => get_top_ancestor_id(),
-        'title_li' => ''
-      )
-      ?>
-      <?php wp_list_pages($args) ?>
+    <?php if ( has_children() OR $post->post_parent > 0 ) { ?>
+        <nav class="site-nav children-links clearfix">
+        <span class="parent-link"><a href="<?php echo get_the_permalink(get_top_ancestor_id()) ?>"><?php echo get_the_title(get_top_ancestor_id()) ?></a></span>
+        <ul>
+          <?php
+            $args = array(
+              'child_of' => get_top_ancestor_id(),
+              'title_li' => ''
+            )
+            ?>
+            <?php wp_list_pages($args) ?>
+        </ul>
+      </nav>
+      <?php } ?>
       <div class="title-column">
         <h2><?php the_title(); ?></h2>
       </div>
