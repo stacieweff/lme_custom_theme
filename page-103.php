b44ab3ac1628ccb202880d62a21290af3a51ae6c
weffelms <stacie.weffelmeyer@wwt.com>
2019-09-09 21:00:12 -0500

    misc changes

---
+++ page-103.php
@@ -0,0 +1,28 @@
+<?php
+
+get_header();
+
+if (have_posts()) :
+  while (have_posts()) : the_post(); ?>
+
+  <article class="post page">
+    <div class="content-wrapper-white">
+      <div class="title-column">
+        <h2><?php the_title(); ?></h2>
+      </div>
+      <div class="text-column">
+        <?php the_content(); ?>
+      </div>
+    </div>
+    <div class="background-image-stripe"> </div>
+  </article>
+
+  <?php endwhile;
+
+else :
+  echo '<p>No content found</p>';
+
+endif;
+
+get_footer();
+?>
