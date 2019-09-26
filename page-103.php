714bd0481f3249889519e8009933e7f3caa440cb
weffelms <stacie.weffelmeyer@wwt.com>
2019-09-26 00:07:31 -0500

    contact us information header

--- page-103.php
+++ page-103.php
@@ -10,6 +10,24 @@ if (have_posts()) :
       <div class="title-column">
         <h2><?php the_title(); ?></h2>
       </div>
+      <div class="contact-us-section">
+          <div class="contact-info">
+            <i class="fas fa-phone contact-icon"></i>
+            <span class="contact-data">St. Louis Metro Area:<br>636-332-6985<br><br>Toll Free: 800-325-8509</span>
+          </div>
+          <div class="contact-info">
+            <i class="fas fa-fax contact-icon"></i>
+            <span class="contact-data">Fax:<br>636-332-8046</span>
+          </div>
+          <div class="contact-info">
+            <i class="fas fa-map-marker-alt contact-icon"></i>
+            <span class="contact-data">104 Mullach Court<br>Suite 1028<br>Wentzville, MO 63385</span>
+          </div>
+          <div class="contact-info">
+            <i class="fas fa-envelope contact-icon"></i>
+            <span class="contact-data">Diane Moore<br>Email: Dmoore1246@aol.com</span>
+          </div>
+      </div>
       <div class="text-column">
         <?php the_content(); ?>
       </div>
